7d402f63356053733774f37e4578415561199196
dawsonfbroadbent <dawsonfbroadbent@gmail.com>
2026-04-01 10:08:51 -0600

    Publish3

--- backend/Mission11.API/Program.cs
+++ backend/Mission11.API/Program.cs
@@ -3,35 +3,35 @@ using Mission11.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddCors(options => options.AddPolicy("AllowReactApp", policy =>
-{
-    policy.WithOrigins("http://localhost:3000", "https://gray-meadow-09abf901e.7.azurestaticapps.net")
-           .AllowAnyHeader()
-           .AllowAnyMethod();
-}));
 
-builder.Services.AddDbContext<BookstoreContext>(options => 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "https://gray-meadow-09abf901e.7.azurestaticapps.net"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<BookstoreContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("BookstoreConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseCors("AllowReactApp");
-
 app.UseHttpsRedirection();
-
+app.UseCors("AllowReactApp");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
