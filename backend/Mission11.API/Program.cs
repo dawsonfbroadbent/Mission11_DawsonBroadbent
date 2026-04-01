84866dfa989337ff330079e5bcd0bd645a236d7d
dawsonfbroadbent <dawsonfbroadbent@gmail.com>
2026-04-01 09:43:50 -0600

    BackendDeploy2

--- backend/Mission11.API/Program.cs
+++ backend/Mission11.API/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddCors(options => options.AddPolicy("AllowReactApp", policy =>
 {
-    policy.WithOrigins("http://localhost:3000")
+    policy.WithOrigins("http://localhost:3000", "https://gray-meadow-09abf901e.7.azurestaticapps.net/")
            .AllowAnyHeader()
            .AllowAnyMethod();
 }));
